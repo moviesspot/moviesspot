a4358943f432ca85c1412d160854538ad359409d
rzmovie <110188620+rzmovie@users.noreply.github.com>
2022-07-29 00:38:08 +0800

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+    'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51',
+    'https://inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994',
+    'https://inquiryclank.com/f2mnb80ep?key=c90b9940c19a9a3b1f8865dc628e8eca',
+    'https://inquiryclank.com/sgnfjfg1?key=cafae42abf7afad6a696828e19405747',
+    'https://scareshortnovel.com/k4qn8fta?key=53a079ee752630cb47b753228c9edbd6',
+    'https://scareshortnovel.com/wz47w7vjp?key=9418bef2825106a98a6d0ea3586504b1',
+    'https://www.highperformancegate.com/u843uwte?key=3e73b89a9ed9d2e16a5d193c60980d28',
+    'https://www.highperformancegate.com/xwu3p1akj?key=063019d69916cb13dc2a409d4de74368',
+    'https://aliveperjuryruby.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31',
+    'https://aliveperjuryruby.com/n3rygrvt?key=a754d241514b7a41541b5461ce5e5e4e'
+    
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
